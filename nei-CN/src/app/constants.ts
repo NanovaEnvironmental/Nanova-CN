bdb8febef20117ddbdd9fc3d4aba78ed7dc84b31
Xiao Zhang <xz9rc@umsystem.edu>
2021-10-20 16:11:56 -0500

    footer added

--- nei-CN/src/app/constants.ts
+++ nei-CN/src/app/constants.ts
@@ -1,2 +1,8 @@
 export const LOGO_IMG = 'assets/Nanova-Env-logo.png';
+export const LOGO_FOOT = 'assets/Nei-footer-logo.png';
 export const TEL = '+1(573)476-6335';
+export const FACEBOOK_IMG = 'assets/Facebook.png';
+export const LINKEDIN_IMG = 'assets/Linkedin.png';
+export const TWITTER_IMG = 'assets/Twitter.png';
+export const YOUTUBE_IMG = 'assets/YouTube.png';
+export const COPY_RIGHT = 'Copyright © 2021 Nanova Environmental, Inc. All Rights Reserved.'
--- nei-CN/src/app/shared/footer/footer.component.ts
+++ nei-CN/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { COPY_RIGHT, FACEBOOK_IMG, LINKEDIN_IMG, LOGO_FOOT, TWITTER_IMG, YOUTUBE_IMG } from 'src/app/constants';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  footImg = LOGO_FOOT;
+  twitterImg = TWITTER_IMG;
+  youTubeImg = YOUTUBE_IMG;
+  faceBookImg = FACEBOOK_IMG;
+  linkedInImg = LINKEDIN_IMG;
+  copyRight = COPY_RIGHT;
+
 
   constructor() { }
 
