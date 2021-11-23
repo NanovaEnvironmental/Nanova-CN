57a1ca8b8a3335f075a0cc53b31099fb5318237e
Xiao Zhang <xz9rc@umsystem.edu>
2021-11-23 16:20:42 -0600

    display service pages

--- nei-CN/src/app/views/contact/contact.module.ts
+++ nei-CN/src/app/views/contact/contact.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RequestQuoteComponent } from './request-quote/request-quote.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MaterialModule } from "src/app/material.module";
 
 @NgModule({
   declarations: [
     RequestQuoteComponent,
     ContactUsComponent
+  ],
+  imports: [
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class ContactModule { }
